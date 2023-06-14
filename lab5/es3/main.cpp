27a8067e3eeb6456b71d8cda912bcad5c721ee6d
LUCA ERBI s319024 <s319024@studenti.polito.it>
2023-06-14 17:53:22 +0200

    v1

--- lab5/es3/main.cpp
+++ lab5/es3/main.cpp
@@ -33,37 +33,48 @@ int main()
         return 1;
     }
 
-    if (pid1 == 0) {
+    if (pid1 == 0)
+    {
 
-        // Esegui il processo figlio
+        // FIGLIO 1
 
-        close(pipePC1[1]); // close write
-        close(pipePC2[1]); // close write
-        char buff[4];
+        close(pipePC1[0]); // close read
 
-        while (1)
-        {
-            read(pipePC[0], buff, sizeof(char) * 4);
-            int cont = 0;
-            while (*(shared_data + cont) != 3)
-            {
-                if (*(shared_data + cont) == 32 || *(shared_data + cont) == 10)
-                    std::cout << (char)*(shared_data + cont);
-                else
-                    std::cout << (char)(*(shared_data + cont) - 32);
-                cont++;
+        while (true) {
+            sleep(WAIT_TIME_1);
+            srand(getppid() + 23);
+            char *message = (char *)malloc((STR_SIZE + 1) * sizeof(char));
+            for (int i = 0; i < STR_SIZE; i++) {
+                int k = rand() % (124 - 97 + 1) + 97;
+                if (k == 123)
+                    k = 32; // space
+                else if (k == 124)
+                    k = 10; // new line
+                message[i] = ((char)k);
             }
-            std::cout << std::endl;
-            int n = rand() % N;
-            if (n == 0)
-            {
-                kill(getppid(), SIGKILL);
-                break;
-            }
-            else
-            {
-                char *message = (char *)malloc((n + 1) * sizeof(char));
-                for (int i = 0; i < n; i++)
+            message[STR_SIZE] = 3;
+            std::cout <<"figlio "<<message << std::endl;
+            write(pipePC1[1], message, sizeof(message));
+            free(message);
+        }
+    }
+    else {
+
+        pid_t pid2 = fork();
+        if (pid2 == -1) {
+            perror("fork");
+            return 1;
+        }
+
+        if (pid2 == 0) {
+            // FIGLIO 2
+            close(pipePC2[0]); // close read
+
+            while (true) {
+                sleep(WAIT_TIME_2);
+                srand(getppid() + 77);
+                char *message = (char *)malloc((STR_SIZE + 1) * sizeof(char));
+                for (int i = 0; i < STR_SIZE; i++)
                 {
                     int k = rand() % (124 - 97 + 1) + 97;
                     if (k == 123)
@@ -72,79 +83,28 @@ int main()
                         k = 10; // new line
                     message[i] = ((char)k);
                 }
-                message[n] = 3;
-                strncpy(shared_data, message, sizeof(char) * (n + 1));
-                std::cout << "figlio genera->" << message << std::endl;
+                message[STR_SIZE] = 3;
+                std::cout <<"figlio "<<message << std::endl;
+                write(pipePC2[1], message, sizeof(message));
                 free(message);
-                write(pipeCP[1], "done", sizeof(char) * 4);
             }
-        }
-    }
-    else {
-
-        pid_t pid2 = fork();
-        if (pid2 == -1)
-        {
-            perror("fork");
-            return 1;
-        }
-
-        if (pid2 == 0) {
-            //FIGLIO 2
         }
         else {
             // PADRE
-            close(pipePC1[1]); // close read
+            close(pipePC1[1]); // close write
             close(pipePC2[1]); // close write
-            char buff[4];
+
             while (1)
             {
-                int n = rand() % N;
-                if (n == 0)
-                {
-                    kill(pid, SIGKILL);
-                    break;
-                }
-                else
-                {
-                    char *message = (char *)malloc((n + 1) * sizeof(char));
-                    for (int i = 0; i < n; i++)
-                    {
-                        int k = rand() % (124 - 97 + 1) + 97;
-                        if (k == 123)
-                            k = 32; // space
-                        else if (k == 124)
-                            k = 10; // new line
-                        message[i] = ((char)k);
-                    }
-                    message[n] = 3;
-                    strncpy(shared_data, message, sizeof(char) * (n + 1));
-                    std::cout << "padre genera->" << message << std::endl;
-                    free(message);
-                    write(pipePC[1], "done", sizeof(char) * 4);
-                }
-                read(pipeCP[0], buff, sizeof(char) * 4);
-                int cont = 0;
-                while (*(shared_data + cont) != 3)
-                {
-                    if (*(shared_data + cont) == 32 || *(shared_data + cont) == 10)
-                        std::cout << (char)*(shared_data + cont);
-                    else
-                        std::cout << (char)(*(shared_data + cont) - 32);
-                    cont++;
-                }
-                std::cout << std::endl;
+                char *message = (char *)malloc((STR_SIZE + 1) * sizeof(char));
+                read(pipePC1[0], message, sizeof(message));
+                std::cout <<"padre "<<message << std::endl;
+
+                read(pipePC2[0], message, sizeof(message));
+                std::cout <<"padre "<<message << std::endl;
+                free(message);
             }
-
-            // Rimuovi l'attaccamento del segmento di memoria condiviso
-            shmdt(shared_data);
-
-            close(pipePC[1]);
-            close(pipeCP[0]);
-            // Rimuovi il segmento di memoria condiviso
-            shmctl(shmid, IPC_RMID, nullptr);
         }
     }
-
     return 0;
-}
+}   
